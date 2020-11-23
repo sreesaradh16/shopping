2b37f53595394da8f25d0627bfcdc9ae8cdf7598
sreesaradh16 <sreesaradh@gmail.com>
2020-11-23 15:04:38 +0530

    project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -111,7 +111,7 @@
 
         <!-- new items -->
 
-        <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel" style="height:340px;">
+        <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
