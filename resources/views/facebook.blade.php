8e4bb4f9e16b326bd641c59e66f07bc3ef1d2223
sreesaradh16 <sreesaradh@gmail.com>
2020-11-23 09:47:19 +0530

    project

--- resources/views/facebook.blade.php
+++ resources/views/facebook.blade.php
@@ -6,11 +6,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Online shopping for Mobiles and Laptops</title>
         <!-- CSS -->
-        <link rel="stylesheet" href="C:\Users\DELL\Desktop\Assignment\bootstrap-4.5.2-dist\css\bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
         <!-- jQuery and JS bundle w/ Popper.js -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        
         <link rel="stylesheet" href="{{asset('css/index.css')}}">
         <link rel="stylesheet" href="{{asset('css/index.js')}}">
 
@@ -198,13 +199,13 @@
                                 <div class="col-md-2"></div>
                                 <div class="form-group col-md-4" style="font-size: 13px; margin-top: 5px;">
                                     <label style="color: white;" for="email">Email</label>
-                                    <input type="email" class="box" id="email">
+                                    <input type="email" id="email">
                                     <span id="email_msg" style="color: red; visibility: hidden;">Enter your
                                         email</span>
                                 </div>
                                 <div class="form-group col-md-4" style="font-size: 13px; margin-top: 5px;">
                                     <label style="color: white;" for="password">Password</label><br>
-                                    <input type="password" class="box" id="password"><br>
+                                    <input type="password" id="password"><br>
                                     <span id="password_msg" style="color: red; visibility: hidden;">Enter your
                                         password</span>
                                 </div>
