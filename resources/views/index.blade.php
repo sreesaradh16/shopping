b9b2a55191758dbb00d521c3b433e757cd97f26e
sreesaradh16 <sreesaradh@gmail.com>
2020-11-23 15:13:49 +0530

    project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online shopping for Mobiles and Laptops</title>
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+   
     <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
@@ -145,20 +145,20 @@
             <div class="row">
                 <div class="col-md-4 boxpad">
                     <div class="box2 bgcolor">
-                        <h4>MONEY BACK</h4>
-                        <p>30 DAY MONEY BACK GUARANTEE</p>
+                    <center><h4>MONEY BACK</h4></center>
+                    <center><p>30 DAY MONEY BACK GUARANTEE</p></center>
                     </div>
                 </div>
                 <div class="col-md-4 boxpad">
                     <div class="box2 bgcolor">
-                        <h4>SPECIAL SALE</h4>
-                        <P>ALL ITEMS UP TO 20% OFF</P>
+                    <center><h4>SPECIAL SALE</h4></center>
+                    <center><P>ALL ITEMS UP TO 20% OFF</P></center>
                     </div>
                 </div>
                 <div class="col-md-4 boxpad">
                     <div class="box2 bgcolor">
-                        <h4>FREE SHIPPING</h4>
-                        <p>FREE SHIP-ON ORDER OVER RS:600</p>
+                    <center><h4>FREE SHIPPING</h4></center>
+                    <center><p>FREE SHIP-ON ORDER OVER RS:600</p></center>
                     </div>
                 </div>
             </div>
